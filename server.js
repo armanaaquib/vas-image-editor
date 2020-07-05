aa46a4623646a84c60e765bc3a02cca1f6185a54
Shankara <=>
2020-07-05 08:31:18 +0530

    added server

--- server.js
+++ server.js
@@ -1,33 +1,74 @@
 const express = require('express');
 const fileUpload = require('express-fileupload');
-const qs = require('querystring');
+const http = require('http');
 const redis = require('redis');
 
 const app = express();
 const client = redis.createClient({ db: 1 });
-let job_id = 1;
+let current_job_id = 1;
 
 app.use(fileUpload());
 app.use(express.urlencoded({ extended: true }));
+
+const fsOptions = {
+  hostname: "127.0.0.1",
+  port: 5000,
+  path: '/save',
+  method: 'POST'
+};
+
+const sendRequest = function (file, operation) {
+  const job_id = current_job_id++;
+  const request = http.request({
+    ...fsOptions, headers: {
+      'Content-type': file.mimetype
+    }
+  }, (res) => {
+    res.on('data', data => {
+      const fieldValues = [
+        "pushed_at", new Date(),
+        "operation", operation,
+        "status", "in_queue",
+        "original_name", data.toString()
+      ];
+
+      client.hmset(`JOB_${job_id}`, fieldValues, () => {
+        client.rpush("jobs", `JOB_${job_id}`);
+      });
+    });
+  });
+
+  request.end(file.data);
+  return job_id;
+};
 
 app.get('/', (req, res) => {
   res.sendFile(`${__dirname}/index.html`);
 });
 
 app.post('/edit', (req, res) => {
+  const response = { id: [] };
+  req.files.file = [].concat(req.files.file);
   req.files.file.forEach(file => {
-    const data = {
-      filename: file.name,
-      data: file.data
+    if (/image/.test(file.mimetype)) {
+      const job_id = sendRequest(file, req.body.operation);
+      response.id.push(job_id);
     }
-    const request = https.request(fsOptions, (res) => {
-      const job_id = job_id++;
-      res.on('data', data => console.log(data.toString()));
-      client.hmset(`JOB_${job_id}`, ["pushed_at", new Date(), "operation", req.body.operation, "status", "in_queue", "original_file", res.body.name]);
-      client.rpush("jobs", `JOB_${job_id}`);
-    });
-    request.write(qs.stringify(data));
-    request.end();
+  })
+  res.end(JSON.stringify(response));
+});
+
+app.get('/status/:job_id', (req, res) => {
+  const id = req.params.job_id;
+  client.hget(`JOB_${id}`, "status", (err, out) => {
+    status = out, path = null;
+    if (out == 'completed') {
+      client.hget(`JOB_${id}`, "modified_name", (err, path) => {
+        res.end(JSON.stringify({ status: out, path }));
+      })
+    } else {
+      res.end(JSON.stringify({ status, path }));
+    }
   });
 });
 
